e70605bc5a6c3856210a5d2325601f794bba9e21
keegan-patterson <logmein.adachi@gmail.com>
2024-11-21 22:13:10 -0800

    Version Reporting and Hello World

---
+++ timetracker.cpp
@@ -0,0 +1,15 @@
+// Main body of TimeTracker app, currently Hello World
+#include <iostream>
+
+#include "timetracker_config.h"
+
+int main(int argc, char* argv[]){
+    // report version
+    std::cout << argv[0] << " Version " << TimeTracker_VERSION_MAJOR << "."
+              << TimeTracker_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
